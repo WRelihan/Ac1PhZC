da109ad44d6dc94dc01acb8398ad04682b1247ec
Wrelihan <willemr68@gmail.com>
2026-05-14 15:55:31 +0200

    Move zero cross interrupt to Emonwar class

---
+++ secrets.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// WiFi
+#define SECRET_WIFI_SSID            "RelihanMesh"
+#define SECRET_WIFI_PASSWORD        "RelMtp6872#"
+
+// UBServe MQTT broker
+#define SECRET_MQTT_HOST_UBSERVE    IPAddress(10, 0, 0, 80)
+#define SECRET_MQTT_PORT_UBSERVE    1884
+#define SECRET_BROKER_USER_UBSERVE  "WilUBMos"
+#define SECRET_BROKER_PASS_UBSERVE  "WilUBMos68#"
+
+// Alternative MQTT broker
+#define SECRET_MQTT_HOST_ALT        IPAddress(10, 0, 0, 168)
+#define SECRET_MQTT_PORT_ALT        1883
+#define SECRET_BROKER_USER_ALT      "WilMos"
+#define SECRET_BROKER_PASS_ALT      "WilMos68#"
