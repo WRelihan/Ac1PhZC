036866cdb2c7afe2666920fa96aef814dcacaff1
Wrelihan <willemr68@gmail.com>
2026-05-16 13:04:07 +0200

    Update version script to include history

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
+
 VERSION=$(git describe --tags --always)
-echo "#ifndef VERSION_H" > version.h
-echo "#define VERSION_H" >> version.h
-echo "const char version[] = \"$VERSION\";" >> version.h
-echo "#endif" >> version.h
+DATE=$(date '+%Y-%m-%d')
+
+HISTORY=$(git tag -l --sort=-version:refname | while read tag; do
+    MSG=$(git tag -l -n1 "$tag" | sed "s/^$tag *//")
+    echo " *  $tag  -  $MSG"
+done)
+
+cat > version.h << EOF
+#ifndef VERSION_H
+#define VERSION_H
+
+const char VERSION[] = "$VERSION";
+
+/*
+ *  Version History
+ *  ---------------
+$HISTORY
+ */
+
+#endif
+EOF
+
+echo "Generated version.h - $VERSION"
